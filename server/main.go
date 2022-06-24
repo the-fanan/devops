e4e416186d6294bc1465bf3f64221c4e1004eed1
Fanan Dala <fanan.dala@yahoo.com>
2022-06-24 13:03:32 +0100

    feat: add simple server

---
+++ server/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "200 OK. Serving: %s\n", r.URL.Path)
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "About Page. Serving: %s\n", r.URL.Path)
+}
+
+func main() {
+	PORT := ":4000"
+	r := http.NewServeMux()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/about", about)
+	err := http.ListenAndServe(PORT, r)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
